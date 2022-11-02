88e3e2c1db206b1aae28d3196e61b2f66dd8b607
Biel-Alves <gabriel_vieira2018@hotmail.com>
2022-11-02 09:08:35 +0100

    starting sanity translation

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "b6240f9da2msh3dacc366de29c4cp184b8cjsn2dd5706f8811",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "42d1d1876e48da629d549f10e1ff02df";
